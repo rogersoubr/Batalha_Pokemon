afaa6823395d6ef2aca50ce16f4e2daac05cfb0a
Roger Brandão <rogersoubrand1a@gmail.com>
2025-11-19 08:27:13 -0300

    :sparkles: feat: fight pokemons

--- batalha.c
+++ batalha.c
@@ -20,7 +20,7 @@ typedef struct Treinador{
 int main(){
     //Pokemon P1, P2;
     Treinador T1, T2;
-    int result, index = 1;//define inicio e final
+    int result, rodada = 1;//define inicio e final
     FILE* arq = fopen("inicio_batalha.txt","r+t");
     if(arq == NULL){
         printf("Nao tem aquivo aqui");
@@ -49,51 +49,73 @@ int main(){
     for (int i = 0; i < T2.quantidade; i++){
         printf("%s %.2f %.2f %.2f %s\n", T2.P[i].nome, T2.P[i].ataque, T2.P[i].defesa, T2.P[i].vida, T2.P[i].tipo);
     }
+    printf("\n");
 
     fclose(arq);
 
-//----------------------------------------------------------------------------------------
+    //----------------------------------------------------------------------------------------
 
-    //FILE* arquivo = fopen("inicio_batalha.txt","w+t");
+    FILE* arquivo = fopen("inicio_batalha.txt","w+t");
     int i =0, j=0;
 
-    while(result){
-        printf("\nDEBUG 1\n");
 
-        do{
-            
-        } while (T1.P[j].vida == 0 || T1.P[j].vida == 0);
+    //tem q batalhar pelo menos uma vez
+    //enquanto algum nao morrer, continua lutando entra si. tenho que isolar os P
+    //comparar os P
+    //quando algum morrer, vai indo ate todos morrerem
+    do{
+        printf("\nRODADA %d \n",rodada);
+        efeito(T1.P[i].nome,T2.P[j].nome, T1.P[i].tipo, T2.P[j].tipo, T1.P[i].ataque, T2.P[j].defesa, T1.P[i].vida ,&T2.P[j].vida);
+
+        if (T2.P[j].vida <= 0){
+            printf("%s matou %s\n",T1.P[i].nome, T2.P[j].nome);
+            j++;
+            continue;//vou ter que usar para comeca outra rodada
+        }
+
+        efeito(T1.P[j].nome,T2.P[i].nome, T1.P[j].tipo, T2.P[i].tipo, T1.P[j].ataque, T2.P[i].defesa, T1.P[j].vida ,&T2.P[i].vida);
+
+        if (T2.P[i].vida <= 0){
+            printf("%s matou %s\n",T1.P[i].nome, T2.P[j].nome);
+            i++;
+            //continue;//vou ter que usar para comeca outra rodada
+        }
+        rodada++;
+
+    } while ((i < T1.quantidade) && (j<T2.quantidade));
+
+    if (i< T1.quantidade){
+        //fprintf();
+        printf("--------------Jogador1 %s venceu !----------\n",T1.nome);
+    }else{
+        printf("--------------Jogador1 %s venceu !----------\n",T2.nome);
+    }
+    
         
         
 
          
-        //enquanto algum nao morrer, continua lutando entra si. tenho quje isolar P
-        for (i ; i < T1.quantidade; i++){
-            for (j ; j < T2.quantidade; j++){
-                printf("\nJogada %d:\n", i+1);
-                dano(T1.P[i].nome,T2.P[j].nome, T1.P[i].tipo, T2.P[j].tipo, T1.P[i].ataque, T2.P[j].defesa, &T2.P[j].vida);
-                printf("\nDEBUG 2\n");
+    /*
+    for (i ; i < T1.quantidade; i++){
+        for (j ; j < T2.quantidade; j++){
+            printf("\nJogada %d:\n", i+1);
+            efeito(T1.P[i].nome,T2.P[j].nome, T1.P[i].tipo, T2.P[j].tipo, T1.P[i].ataque, T2.P[j].defesa, &T2.P[j].vida);
+            printf("\nDEBUG 2\n");
 
-                if (T1.P[j].vida <= 0){
-                    printf("%s matou %s",T1.P[i].nome, T2.P[j].nome);
-                    printf("\nDEBUG 3\n");
-                }
+            if (T1.P[j].vida <= 0){
+                printf("%s matou %s",T1.P[i].nome, T2.P[j].nome);
+                printf("\nDEBUG 3\n");
+            }
                 
-            }
-            if (T1.P[i].vida <= 0){
-                printf("%s matou %s",T2.P[j].nome, T1.P[i].nome);
-                printf("\nDEBUG 4\n");
-            }
+        }
+        if (T1.P[i].vida <= 0){
+            printf("%s matou %s",T2.P[j].nome, T1.P[i].nome);
+            printf("\nDEBUG 4\n");   
             
-            
-        }/*
-        */
-        printf("\nDEBUG 5\n");
-        result =0;
     }
-        
-
-    //fclose(arquivo);
+    */
+    printf("\nDEBUG 5\n");
     
+    //fclose(arquivo);   
     return 0;
 }
--- dano_poder.h
+++ dano_poder.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 /*
 float diferenca_defesa(float defesa2, float* vida2){
         if (defesa2 <0){
@@ -9,42 +10,43 @@ float diferenca_defesa(float defesa2, float* vida2){
 }
 */
 
-//nome atacante, node defensor, tipo atacante, tipo defensor, dano atacante, defesa defensor, vida defensor 
-void efeito( char nome_atacante[100], char nome_defensor[100], char tipo_atacante[100], char tipo_defensor[100], float dano_atacante, float defesa_defensor, float* vida_defensor){
+//nome atacante, node defensor, tipo atacante, tipo defensor, dano atacante, defesa defensor, vidade atacante, vida defensor 
+void efeito( char nome_atacante[100], char nome_defensor[100], char tipo_atacante[100], char tipo_defensor[100], float dano_atacante, float defesa_defensor, float vida_atacante, float* vida_defensor){
+    //comparar minusculi
     //primeiro com vantagem
     if ((tipo_atacante == "eletrico" && tipo_defensor=="agua") ||
         (tipo_atacante == "pedra" && tipo_defensor=="eletrico") ||
         (tipo_atacante == "agua" && tipo_defensor=="fogo") ||
         (tipo_atacante == "fogo" && tipo_defensor=="gelo") ||
-        (tipo_atacante == "gelo" && tipo_defensor=="pedra")){
-        float dano = dano_atacante*1.2
+        (tipo_atacante == "gelo" && tipo_defensor=="pedra")
+    ){
+        float super_efetivo = dano_atacante*1.2;
         //defesa_defensor = defesa_defensor - (dano_atacante*1.2);
-        printf("%s atacou %s com dano de %.2f",nome_atacante,nome_defensor,dano_atacante*1.2);
-        if (defesa2 <0){
-            *vida2 -= defesa2;
-        }else{
-            *vida2 -= 1;//acho que vou ter q mudar para +
-        }
-        if ((dano_atacante*1.2) > defesa_defensor){
-            *vida_defensor -= defesa2;
+        printf("DEBUG 1: %s atacou %s com dano de %.2f\n",nome_atacante,nome_defensor,super_efetivo);
+
+        if (super_efetivo > defesa_defensor){
+            float dano = super_efetivo - defesa_defensor;
+            *vida_defensor -= dano;//testar sem mandar direto
         //diferenca_defesa()
-    }else if(tipo_atacante == "gelo" && tipo_defensor=="pedra"){
-        defesa2 = defesa2 - dano1*1.2;
-        if (defesa2 <0){
-            *vida2 -= defesa2;
-        }else{
-            *vida2 -= 1;//acho que vou ter q mudar para +
         }
-        printf("Pokemon: %s Vida: %.2f \n", nome2, vida2);
+    }else if((tipo_atacante == "agua" && tipo_defensor=="pedra") ||
+        (tipo_atacante == "fogo" && tipo_defensor=="eletrico") ||
+        (tipo_atacante == "gelo" && tipo_defensor=="agua") ||
+        (tipo_atacante == "pedra" && tipo_defensor=="fogo") ||
+        (tipo_atacante == "eletrico" && tipo_defensor=="gelo")){
+      
+        float nao_efetivo = dano_atacante*0.8;
+        printf("DEBUG 2: %s atacou %s com dano de %.2f\n",nome_atacante,nome_defensor,nao_efetivo);
+        if (nao_efetivo > defesa_defensor){
+            float dano = nao_efetivo - defesa_defensor;
+            *vida_defensor -= dano;//testar sem mandar direto
+        //diferenca_defesa()
+        }
 
     }else{
-        defesa2 = defesa2 - dano1;
-        if (defesa2 <0){
-            *vida2 -= defesa2;
-        }else{
-            *vida2 -= 1;//acho que vou ter q mudar para +
-        }
-        printf("Pokemon: %s Vida: %.2f \n", nome2, vida2);
+        printf("%s atacou %s com dano de %.2f\n",nome_atacante,nome_defensor,1); 
+        *vida_defensor -= 1;//testar sem mandar direto
     }
+    printf("vida %s, %.2f |VS| vida %s, %.2f\n", nome_defensor, vida_defensor, nome_atacante, vida_atacante);
     
 }
